e4e9d25da285c0221517a47de4f16d3d7bd96eb2
thanaponlp57 <75792634+thanaponlp57@users.noreply.github.com>
2023-06-02 03:34:46 +0700

    Update default-image.directive
    Make it easier to read

--- src/app/directives/default-image.directive.ts
+++ src/app/directives/default-image.directive.ts
@@ -5,26 +5,28 @@ import { Directive, Input, HostBinding, AfterViewInit } from '@angular/core';
 })
 export class DefaultImageDirective implements AfterViewInit {
   @HostBinding('src')
-  @Input()
-  src!: string;
+  @Input() src!: string;
 
-  @Input()
-  appDefaultImage!: string;
+  @Input() appDefaultImage!: string;
 
   async ngAfterViewInit(): Promise<void> {
-    const canDisplay =  await this.loadImage(this.src);
+    const canDisplay = await this.loadImage(this.src);
 
     if (!canDisplay) {
-      this.src = this.appDefaultImage;
+      this.setSrc(this.appDefaultImage);
     }
   }
 
-  async loadImage(url: string): Promise<boolean> {
+  private loadImage(url: string): Promise<boolean> {
     return new Promise((resolve) => {
       const image = new Image();
       image.onload = () => resolve(true);
       image.onerror = () => resolve(false);
       image.src = url;
     });
+  }
+
+  private setSrc(src: string): void {
+    this.src = src;
   }
 }
